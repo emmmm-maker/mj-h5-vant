1e39c4092189828ef84500307438f5933b49d69c
xingchaochao <xing_chaochao@sina.com>
2023-05-09 15:46:01 +0800

    feat:项目初始化-vant组件库引入

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
     app
+    <van-button type="primary">主要按钮</van-button>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+
+}
+</script>
 <style lang="less">
 
 </style>
